4a16f4657c1e6bca71d13b275ddd43ea03a69b44
honeytech <honey93k@gmail.com>
2022-12-12 18:56:22 -0800

    added comments to server.js

--- server.js
+++ server.js
@@ -4,26 +4,27 @@ const db = require("./db/db.json");
 const idForNote = require("uniqid");
 const fs = require("fs");
 
-const myApp = express();
-const PORT = process.env.PORT || 3001;
+const myApp = express(); 
+const PORT = process.env.PORT || 3001; // reserve free PORT 3001
 
 myApp.use(express.urlencoded({extended:true}));
 myApp.use(express.json());
 
 myApp.use(express.static("public"));
 
+// "GET" route to index.html with
 myApp.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
-
+// "GET" route to notes.html with
   myApp.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
-
+// "GET" route for reading and returning notes from the db.json file 
 myApp.get("/api/notes/", (req, res) => {
     res.json(db);
   });
-
+// "POST route that saves new notes to request body,adds it to db.json and returns the new note to user"  
   myApp.post("/api/notes", (req, res) => {
     console.log(`${req.method} request received`);
     const { title, text } = req.body;
@@ -44,7 +45,7 @@ myApp.get("/api/notes/", (req, res) => {
       res.json(response);
     }
   });
-
+// "DELETE" route to delete old notes  
   myApp.delete("/api/notes/:id", (req, res) => {
     const { id } = req.params;
     const oldNote = db.findIndex((note) => note.id === id);
